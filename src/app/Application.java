b1a3f1fd67a09067da31f0d9ee4f8947ec006d3a
ThePrometheus <nazartkach3@gmail.com>
2018-02-18 17:25:01 +0200

    Start config

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,8 +1,27 @@
 package app;
+import javax.swing.*;
 
 /**
  * Created by root on 18.02.18.
  */
 public class Application {
+    public final static Application self = new Application();
+
+    // REPOSITORIES
+
+
+    //SERVICES
+
+
+    //CONTROLLERS
+
+
+    //GENERAL BEHAVIOUR
+    public static void  showMessage(String message){
+        JOptionPane.showMessageDialog(null,message);
+
+    }
+
+
 
 }
---
+++ src/app/Constants.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by root on 18.02.18.
+ */
+
+    public class Constants {
+
+        //constants themselves
+        public static final int DB_SUCCESS_EXECUTION_CODE = 1;
+    }
+
+
