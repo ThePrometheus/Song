1cd9946ab3743ddbc3db74d8b7a6674a3a5e710e
ThePrometheus <nazartkach3@gmail.com>
2018-02-18 17:33:19 +0200

    Ended with preliminary config with app package

---
+++ src/app/DBConnector.java
@@ -0,0 +1,49 @@
+package app;
+
+import java.sql.*;
+
+/**
+ * Created by root on 18.02.18.
+ */
+public class DBConnector {
+    public static DBConnector shared = new DBConnector();
+
+    private static final String CONN_URL = "jdbc:mysql://localhost/studio?"+"user=root&password=root";
+
+    private Connection connection = null;
+
+
+    private DBConnector() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(CONN_URL);}
+
+catch(ClassNotFoundException e){
+            e.printStackTrace();
+
+
+            }
+            catch (SQLException e){
+    e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void finalize(){
+        try {
+            connection.close();
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public Connection getConnect(){
+        return connection;
+    }
+
+
+
+
+
+}
---
+++ src/app/Strings.java
@@ -0,0 +1,21 @@
+package app;
+
+/**
+ * Created by root on 18.02.18.
+ */
+public class Strings {
+    public static final String MENU_SONG = "Song";
+
+    public static final  String EDIT = "Edit";
+
+    public static final String NEW = "New";
+
+    public static final String MUSICIAN = "Musician";
+
+    public static final String ADD_MUSICIAN = "Add musician";
+
+    public static final String SHARE = "Share";
+    public static final String CHANGE_SHARE = "Change share";
+
+
+}
